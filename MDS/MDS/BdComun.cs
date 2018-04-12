f2849705024c90d8ee2cd0e530de374bb2a65995
Unknown <contactos503@gmail.com>
2018-04-11 21:38:17 -0600

    conexion terminada-

--- MDS/MDS/BdComun.cs
+++ MDS/MDS/BdComun.cs
@@ -16,23 +16,7 @@ namespace MDS
         private DataSet dst;
         private MySqlDataAdapter adaptador;
 
-        public void Conexion()
-        {
-            try
-            {
-                conex = @"Server=205.134.252.210;Database=mdsxyz_mdsbd; Uid=mdsxyz_user; Pwd=carmando6; port=3306; ";
-                conectar = new MySqlConnection(conex);
-                conectar.Open();
-
-                MessageBox.Show("conexion exitosa");
-                
-            }
-            catch(Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-
-        }
+        
         public static MySqlConnection ObtenerConexion()
         {
             MySqlConnection conectar = new MySqlConnection("Server=205.134.252.210;Database=mdsxyz_mdsbd; Uid=mdsxyz_user; Pwd=carmando6; port=3306;");
